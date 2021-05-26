27d812b0f760a491385ae6af40a18864f36fa51e
Reet Dubey <55314761+ReetDubey@users.noreply.github.com>
2021-05-27 01:45:17 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import Display from "./Display"
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+
+ReactDOM.render(<Display />, document.getElementById("root"));
+
